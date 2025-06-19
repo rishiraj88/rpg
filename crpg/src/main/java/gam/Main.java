7a12876aad2ffbac0504c5a75c30b38851db0b2b
Rishi Raj <rishiraj88+github@gmail.com>
2025-06-19 22:31:09 +0200

    Make code in Main class better modularized.

--- crpg/src/main/java/gam/Main.java
+++ crpg/src/main/java/gam/Main.java
@@ -6,17 +6,26 @@ import gam.util.Factory;
 public class Main {
     public static void main(String[] args) {
         //1. Initialize game server.
-        GameServer gameServer = (GameServer) Factory.get("gam.GameServer",true);
+        Thread gameServerThread = startServer();
+        //2. Start game client and connect to server.
+        startClient(gameServerThread);
+    }
+
+    static Thread startServer() {
+        GameServer gameServer = (GameServer) Factory.get("gam.GameServer", true);
         Thread gameServerThread = new Thread(() -> gameServer.initGamePlay());
         gameServerThread.start();
+        return gameServerThread;
+    }
 
-        //2. Start game client and connect to server.
+    static void startClient(Thread gameServerThread) {
         Thread gameClientThread = new Thread(() -> {
             try {
                 gameServerThread.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+            GameServer gameServer = (GameServer) Factory.get("gam.GameServer", false);
             ((GameClient) Factory.get("gam.client.GameClient")).connectToServer(gameServer);
         });
         gameClientThread.start();
